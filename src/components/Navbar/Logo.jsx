43b9b5633abe33a8e95fab4d73aba3c6c8b72da9
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-13 13:34:52 -0800

    'Main'

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
 	return (
 		<Link to="/">
-			<img src="/images/logo.webp" alt="logo" width={128} height={128} />
+			<img src="https://pouch.jumpshare.com/preview/Di8k3ZNJOi4pkkXuYMICAGPAfK0VolpcW-lcYE0FcbWgWwZYGVB6tITibZYyh90eG6VUx_dxiJyiSdRNGRUOF2ywGKrs-WuxrAicYDXr8ic" alt="logo" width={128} height={128} />
 		</Link>
 	);
 }
--- src/pages/Home/components/PolicySection.jsx
+++ src/pages/Home/components/PolicySection.jsx
@@ -3,7 +3,7 @@ import CustomTextImgSection from "../../../components/CustomTextImgSection";
 export default function PolicySection({ t }) {
 	return (
 		<CustomTextImgSection
-			imgUrl="/images/policy.webp"
+			imgUrl="https://pouch.jumpshare.com/preview/nOJVvzIJqa8EQVS-Z45A8SC7pj7Jh-8x7nEfbAm0cb2YemiGEedLW24H06wVq8exG6VUx_dxiJyiSdRNGRUOF-WYXa5MoNBfFktDFUCm_2M"
 			titlePrefix={t("home.policy.titlePrefix")}
 			title={t("home.policy.title")}
 			description={t("home.policy.description")}
