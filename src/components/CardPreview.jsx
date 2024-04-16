ddc94dab080f72f713ea9f855470de74b383c5c2
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-16 13:19:57 -0800

    main

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -14,24 +14,24 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
     window.location.href = des
    }
   return (
-    <div className="min-w-[270px] justify-center flex  max-w-[480px]  h-[200px] mx-auto md:mb-3 xs:mb-10 md:mb-3">
+    <div className="min-w-[270px] justify-center flex  max-w-[480px]  h-[180px] mx-auto md:mb-3 xs:mb-10 md:mb-3">
       <figure
-        className="relative  flex justify-center shrink-0 mx-auto  h-[200px]  w-[100%]  rounded-2xl  overflow-hidden transition-colors"
+        className="relative  flex justify-center shrink-0 mx-auto  h-[180px]  w-[100%]  rounded-2xl  overflow-hidden transition-colors"
         style={{ backgroundColor: cardSitting.color }}
       >
         <div
-          className={" w-[100%]      h-[200px] "}
+          className={" w-[100%]      h-[180px] "}
           style={{ position: "relative", maxWidth: "480px" }}
         >
           {!back ? (
             <>
-              <div className="absolute w-full     h-[200px]  mx-auto  ">
+              <div className="absolute w-full     h-[180px]  mx-auto  ">
                 {cardSitting.shape ? (
                   <img
                     src={cardSitting.shape}
                     height={200}
                     alt="gift card"
-                    className="rounded-2xl  object-fill mx-auto  w-full h-[200px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                    className="rounded-2xl  object-fill mx-auto  w-full h-[180px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                   />
                 ) : null}
               </div>
