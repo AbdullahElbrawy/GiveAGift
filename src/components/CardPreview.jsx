b03798cf592459096c7cf1796c5c9aa8d330034d
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-16 12:36:32 -0800

    main

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -44,7 +44,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
                     alt={`${cardSitting.brand.logoName} logo`}
                     width={65}
                     height={65}
-                    className="rounded-2xl object-fill mix-blend-multiply bg-white bg-blend-multiply transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                    className="rounded-full object-fill mix-blend-multiply bg-white bg-blend-multiply transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                   />
                 ) : null}
               </div>
