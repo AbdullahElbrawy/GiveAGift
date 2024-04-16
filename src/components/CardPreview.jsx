c84342ad1f77c515a07b2e09a9b1fe007bc4730f
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-16 11:11:08 -0800

    main

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -15,13 +15,13 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
     window.location.href = des
    }
   return (
-    <div className="w-min-[280px] lg:w-[450px]   w-full ">
+    <div className="min-w-[280px]  lg:w-[450px]   w-full ">
       <figure
         className="relative  flex justify-center shrink-0 mx-auto   h-[180px] md:h-[240px] md:w-[450px]  rounded-2xl  overflow-hidden transition-colors"
         style={{ backgroundColor: cardSitting.color }}
       >
         <div
-          className={"   lg:w-[450px] mx-auto w-full  w-min-[280px]    md:h-[240px]   h-[180px] "}
+          className={"   lg:w-[450px] mx-auto w-full min-w-[280px]    md:h-[240px]   h-[180px] "}
           style={{ position: "relative", maxWidth: "480px" }}
         >
           {!back ? (
@@ -32,7 +32,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
                     src={cardSitting.shape}
                     height={200}
                     alt="gift card"
-                    className="rounded-2xl  object-fill mx-auto  w-full   lg:w-[450px]     md:h-[240px]   h-[180px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                    className="rounded-2xl  object-fill mx-auto  min-w-[280px]   lg:w-[450px]     md:h-[240px]   h-[180px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                   />
                 ) : null}
               </div>
