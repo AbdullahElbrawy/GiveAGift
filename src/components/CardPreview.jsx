36cf607d1099cba268d40ab106aaa944c64cf8d1
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-16 11:00:52 -0800

    main

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -21,7 +21,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
         style={{ backgroundColor: cardSitting.color }}
       >
         <div
-          className={"   lg:w-[450px] mx-auto w-full  w-min-[280px]   md:h-[240px]   h-[180px] "}
+          className={"   lg:w-[450px] mx-auto   w-min-[280px]   md:h-[240px]   h-[180px] "}
           style={{ position: "relative", maxWidth: "480px" }}
         >
           {!back ? (
@@ -32,12 +32,12 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
                     src={cardSitting.shape}
                     height={200}
                     alt="gift card"
-                    className="rounded-2xl  object-fill mx-auto  w-full   lg:w-[450px]     md:h-[240px]   h-[180px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                    className="rounded-2xl  object-fill mx-auto    lg:w-[450px]     md:h-[240px]   h-[180px]  object-fill absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                   />
                 ) : null}
               </div>
 
-              <div className={`absolute top-2 mix-blend-multiply  right-2 z-10`}>
+              <div className={`absolute top-2 mix-blend-colorDodge  right-2 z-10`}>
                 {cardSitting.brand.logoName ? (
                   <img
                     src={cardSitting.brand.logoWithoutBackground}
