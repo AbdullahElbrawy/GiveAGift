2f1c7026dc4628351dc6e01c9dc285e20d299bc0
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-17 01:18:27 -0800

    main

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -1,5 +1,6 @@
 import { Avatar, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+
 export default function CardPreview({ cardSitting, back, ShowBrand }) {
   const { t } = useTranslation();
   const fontClassName =
@@ -10,33 +11,35 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
       : cardSitting.font === "Cairo"
       ? "!font-cairo"
       : "";
-   const navToUrl =(des)=>{
-    window.location.href = des
-   }
+
+  const navToUrl = (des) => {
+    window.location.href = des;
+  };
+
   return (
-    <div className="max-w-[450px] min-w-[215px] w-full  sm:w-[450px]   justify-center flex    h-[220px] mx-auto md:mb-3 xs:mb-10 md:mb-3">
+    <div className="aspect-w-4 aspect-h-3 max-w-[450px] min-w-[215px] w-full sm:w-[450px] justify-center flex h-[220px] mx-auto md:mb-3 xs:mb-10 md:mb-3">
       <figure
-        className="relative  flex justify-center shrink-0 mx-auto  h-[220px]  w-[100%]  rounded-2xl  overflow-hidden transition-colors"
+        className="relative flex justify-center shrink-0 mx-auto h-full w-full rounded-2xl overflow-hidden transition-colors"
         style={{ backgroundColor: cardSitting.color }}
       >
         <div
-          className={" w-[100%]   w-full   h-[220px] "}
+          className={"w-full h-full"}
           style={{ position: "relative", maxWidth: "480px" }}
         >
           {!back ? (
             <>
-              <div className="absolute   min-w-[215px]   sm:w-[450px]  h-[220px]   mx-auto  ">
+              <div className="absolute w-full h-full">
                 {cardSitting.shape ? (
                   <img
                     src={cardSitting.shape}
                     height={200}
                     alt="gift card"
-                    className="rounded-2xl  object-fill mx-auto w-full min-w-[215px]  sm:w-[450px]   h-[220px]   absolute  transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                    className="rounded-2xl object-fill w-full h-full absolute transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                   />
                 ) : null}
               </div>
 
-              <div className={`absolute top-2    right-2 z-10`}>
+              <div className={`absolute top-2 right-2 z-10`}>
                 {cardSitting.brand.logoName ? (
                   <div className="rounded-full bg-black bg-opacity-40 p-1">
                     <img
@@ -44,7 +47,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
                       alt={`${cardSitting.brand.logoName} logo`}
                       width={65}
                       height={65}
-                      className="rounded-full object-fill     transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
+                      className="rounded-full object-fill transition duration-500 group-hover:opacity-0 group-hover:scale-75 z-10"
                     />
                   </div>
                 ) : null}
@@ -52,26 +55,30 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
             </>
           ) : (
             <div
-              className="flex flex-col justify-center items-center text-center   p-4 gap-2 h-full"
+              className="flex flex-col justify-center items-center text-center p-4 gap-2 h-full"
               style={{ color: cardSitting.textColor }}
             >
               <Typography
                 variant="h5"
                 component="p"
-                className={`${fontClassName} word-wrap   p-4 transition-colors`}
-                style={{overflowWrap: 'anywhere'}}
+                className={`${fontClassName} word-wrap p-4 transition-colors`}
+                style={{ overflowWrap: "anywhere" }}
               >
                 {cardSitting.message}
               </Typography>
               <Typography
                 variant="h6"
                 component="p"
-                className={`${fontClassName}  p-4 transition-colors`}
+                className={`${fontClassName} p-4 transition-colors`}
               >
                 {cardSitting.price} {t("currency")}
               </Typography>
               <div className="absolute bottom-4 left-4">
-                <img src="https://i.imgur.com/wkyCwby.png" alt="logo" width={100} />
+                <img
+                  src="https://i.imgur.com/wkyCwby.png"
+                  alt="logo"
+                  width={100}
+                />
               </div>
             </div>
           )}
@@ -82,11 +89,15 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
         <div className="flex justify-between items-center w-full px-5 mt-4">
           <h3 className="flex items-center gap-2 text-xl">
             <Avatar
-              src={cardSitting.logoImage ? cardSitting.logoImage: cardSitting.brand.logoWithoutBackground}
+              src={
+                cardSitting.logoImage
+                  ? cardSitting.logoImage
+                  : cardSitting.brand.logoWithoutBackground
+              }
               alt={cardSitting.brand.logoName}
               className="[&_img]:object-contain object-contain cursor-pointer"
-              sx={{ width: 60 , height: 60 }}
-              onClick={()=>navToUrl(cardSitting.brand.brandUrl)}
+              sx={{ width: 60, height: 60 }}
+              onClick={() => navToUrl(cardSitting.brand.brandUrl)}
             />
             {cardSitting.brand.logoName}
           </h3>
