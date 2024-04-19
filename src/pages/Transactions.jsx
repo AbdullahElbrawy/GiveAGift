fa20b518789ee6591cd9f61d69d921cb214451d2
AbdullahElbrawy <abdullahalbrawy00@gmail.com>
2024-04-18 23:07:30 -0800

    main

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -119,7 +119,7 @@ const TransactionsTable = () => {
                 </table>
             </div>
             {/* Pagination Controls */}
-            <div className="pagination gap-3">
+            <div className="pagination flex  gap-5">
                 <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
                 <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
                 <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
